d624bfe274484e5e3cad56b5297dfd340b0ec5c8
李铭昕 <715557344@qq.com>
2020-10-14 14:59:12 +0800

    Added swow to travis.

---
+++ tests/swow.install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+wget https://github.com/swow/swow/archive/"${SW_VERSION}".tar.gz -O swow.tar.gz
+mkdir -p swow
+tar -xf swow.tar.gz -C swow --strip-components=1
+rm swow.tar.gz
+cd swow || exit
+phpize
+./configure
+make -j "$(nproc)"
+make install
