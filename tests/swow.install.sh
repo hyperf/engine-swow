4050058d61b87f4330360b239b6589840d88fd2b
李铭昕 <715557344@qq.com>
2020-10-23 17:18:04 +0800

    Update swow.install.sh

--- tests/swow.install.sh
+++ tests/swow.install.sh
@@ -3,7 +3,7 @@ wget https://github.com/swow/swow/archive/"${SW_VERSION}".tar.gz -O swow.tar.gz
 mkdir -p swow
 tar -xf swow.tar.gz -C swow --strip-components=1
 rm swow.tar.gz
-cd swow || exit
+cd swow/ext || exit
 
 phpize
 ./configure --enable-debug
