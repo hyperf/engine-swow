cd12d39e4eb2aada88b3e12e9f91ed033306ab9f
李铭昕 <715557344@qq.com>
2020-10-30 09:39:48 +0800

    Added WatchDogListener

---
+++ src/Listener/WatchDogListener.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Engine\Listener;
+
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Framework\Event\BootApplication;
+use Swow\WatchDog;
+
+class WatchDogListener implements ListenerInterface
+{
+    public function listen(): array
+    {
+        return [
+            BootApplication::class,
+        ];
+    }
+
+    public function process(object $event)
+    {
+        WatchDog::run();
+    }
+}
---
+++ tests/Cases/WatchDogTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\Cases;
+
+use Hyperf\Engine\Listener\WatchDogListener;
+use PHPUnit\Framework\TestCase;
+use Swow\WatchDog;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class WatchDogTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        if (WatchDog::isRunning()) {
+            WatchDog::stop();
+        }
+    }
+
+    protected function tearDown(): void
+    {
+        if (WatchDog::isRunning()) {
+            WatchDog::stop();
+        }
+    }
+
+    public function testWatchDogIsRunning()
+    {
+        $this->assertFalse(WatchDog::isRunning());
+        (new WatchDogListener())->process(new \stdClass());
+        $this->assertTrue(WatchDog::isRunning());
+    }
+}
