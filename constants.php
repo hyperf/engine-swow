7b0f43e69ccdd175f60d13d7243b4fc6d8028313
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2022-11-09 12:12:59 +0800

    Remove some constants

--- constants.php
+++ constants.php
@@ -9,11 +9,5 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-use Swow\Socket;
-
 ! defined('SWOOLE_HOOK_ALL') && define('SWOOLE_HOOK_ALL', 0);
 ! defined('SWOOLE_BASE') && define('SWOOLE_BASE', 0);
-! defined('SWOOLE_SOCK_TCP') && define('SWOOLE_SOCK_TCP', Socket::TYPE_TCP);
-! defined('SWOOLE_SOCK_TCP6') && define('SWOOLE_SOCK_TCP6', Socket::TYPE_TCP6);
-! defined('SWOOLE_SOCK_UDP') && define('SWOOLE_SOCK_UDP', Socket::TYPE_UDP);
-! defined('SWOOLE_SOCK_UDP6') && define('SWOOLE_SOCK_UDP6', Socket::TYPE_UDP6);
