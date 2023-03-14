ca6cee9144682e607ea5b2953099b2eff5f65dcd
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-03-14 10:30:38 +0800

    Adds `Coroutine::exits()`

--- src/Coroutine.php
+++ src/Coroutine.php
@@ -141,4 +141,12 @@ class Coroutine extends SwowCo implements CoroutineInterface
             'coroutine_num' => static::count(),
         ];
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function exists(int $id):bool
+    {
+        return parent::get($id) !== null;
+    }
 }
