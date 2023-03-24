9cb5ec0db6d86edd29ff0be47921814e74636dea
Deeka Wong <huangdijia@gmail.com>
2023-03-24 18:45:56 +0800

    Adds HTTP Connection (#19)
    Co-authored-by: assert <zhangchengming@kkguan.com>
    Co-authored-by: 李铭昕 <715557344@qq.com>

---
+++ src/Http/WritableConnection.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Engine\Http;
+
+use Hyperf\Engine\Contract\Http\Writable;
+use Swow\Psr7\Server\ServerConnection;
+
+class WritableConnection implements Writable
+{
+    public function __construct(protected ServerConnection $response)
+    {
+    }
+
+    public function write(string $data): bool
+    {
+        $this->response->write([
+            sprintf("%s\r\n%s\r\n", strlen($data), $data),
+        ]);
+
+        return true;
+    }
+
+    /**
+     * @return ServerConnection
+     */
+    public function getSocket(): mixed
+    {
+        return $this->response;
+    }
+
+    public function end(): bool
+    {
+        $this->response->write(["0\r\n", "\r\n"]);
+        return true;
+    }
+}
