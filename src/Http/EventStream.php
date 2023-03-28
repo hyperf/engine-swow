517b210ae00378ff368314cb06ea16994b35c972
张城铭 <z@hyperf.io>
2023-03-28 09:11:04 +0800

    feature: Add support for EventStream (#21)
    Co-authored-by: assert <zhangchengming@kkguan.com>

---
+++ src/Http/EventStream.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Engine\Http;
+
+use Hyperf\Engine\Contract\Http\Writable;
+use Swow\Http\Http;
+use Swow\Psr7\Server\ServerConnection;
+
+class EventStream
+{
+    public function __construct(protected Writable $connection)
+    {
+        /** @var ServerConnection $socket */
+        $socket = $this->connection->getSocket();
+        $socket->write([
+            Http::packResponse(
+                statusCode: 200,
+                headers: [
+                    'Content-Type' => 'text/event-stream; charset=utf-8',
+                    'Transfer-Encoding' => 'chunked',
+                    'Cache-Control' => 'no-cache',
+                ],
+                protocolVersion: '1.1'
+            ),
+        ]);
+    }
+
+    public function write(string $data): self
+    {
+        $this->connection->write($data);
+        return $this;
+    }
+
+    public function end(): void
+    {
+        $this->connection->end();
+    }
+}
