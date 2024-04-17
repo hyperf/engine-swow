e0fdd8ff16f3f7b80734a2b4114650034c813791
李铭昕 <715557344@qq.com>
2024-04-17 21:17:10 +0800

    Added `Hyperf\Engine\Http\Http`

---
+++ src/Http/Http.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Engine\Http;
+
+use Hyperf\Engine\Contract\Http\Http as HttpContract;
+use Stringable;
+use Swow\Http\Http as SwowHttp;
+
+class Http implements HttpContract
+{
+    public static function packRequest(string $method, string|Stringable $uri, array $headers = [], string|Stringable $body = '', string $protocolVersion = HttpContract::DEFAULT_PROTOCOL_VERSION): string
+    {
+        return SwowHttp::packRequest(
+            $method,
+            $uri,
+            $headers,
+            $body,
+            $protocolVersion
+        );
+    }
+
+    public static function packResponse(int $statusCode, string $reasonPhrase = '', array $headers = [], string|Stringable $body = '', string $protocolVersion = HttpContract::DEFAULT_PROTOCOL_VERSION): string
+    {
+        return SwowHttp::packResponse(
+            $statusCode,
+            $reasonPhrase,
+            $headers,
+            $body,
+            $protocolVersion
+        );
+    }
+}
---
+++ tests/Cases/HttpTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Cases;
+
+use Hyperf\Engine\Http\Http;
+use HyperfTest\Cases\AbstractTestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class HttpTest extends AbstractTestCase
+{
+    public function testHttpPackRequest()
+    {
+        $data = Http::packRequest('GET', 'https://www.baidu.com', ['Content-Type' => 'application/json'], 'Hello World');
+
+        $this->assertSame("GET https://www.baidu.com HTTP/1.1\r
+Content-Type: application/json\r
+\r
+Hello World", $data);
+    }
+
+    public function testHttpPackResponse()
+    {
+        $data = Http::packResponse(200, 'OK', ['Content-Type' => 'application/json'], 'Hello World');
+
+        $this->assertSame("HTTP/1.1 200 OK\r
+Content-Type: application/json\r
+\r
+Hello World", $data);
+    }
+}
